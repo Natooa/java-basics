46c0bfbd39748df706c7d8afedd56399ba10b5cb
Nurdiyar <nurdi.instagramm@gmail.com>
2025-07-29 16:10:50 +0500

    Training Comparable and Comparator interface

---
+++ CollectionsAndGenerics/src/main/java/comparableandcomparator/Test1.java
@@ -0,0 +1,17 @@
+package comparableandcomparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Zaur");
+        list.add("Ivan");
+        list.add("Mariya");
+        System.out.println("Before sort " + list);
+        Collections.sort(list);
+        System.out.println("After sort " + list);
+    }
+}
